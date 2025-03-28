0f1b2b4c0f1e3592967fe3e280884c287c9999e3
Charlie Wu <charliewu500@gmail.com>
2025-03-29 03:11:48 +0800

    [add] 細條 HeroBanner，增加圖片與視差滾動

--- golden_days/src/components/home/HeroBanner.tsx
+++ golden_days/src/components/home/HeroBanner.tsx
@@ -1,9 +1,25 @@
 import { Link } from 'react-router-dom';
+import styles from './HeroBanner.module.scss';
+import { useEffect, useRef } from 'react';
 export default function HeroBanner() {
+    const heroRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (!heroRef.current) return;
+            const scrollY = window.scrollY;
+            heroRef.current.style.backgroundPositionY = `${scrollY * 1}px`; // 你可以調整 0.5 -> 越小越「遠」
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
     return (
-        <section style={{ padding: '4rem', textAlign: 'center', background: '#fdf6e3' }}>
-            <h1>金時光</h1>
-            <p>金瓜石、九份、水湳洞的生活風景</p>
+        <section ref={heroRef} className={styles.HeroBanner}>
+            <div>
+                <h1>金時光</h1>
+                <p>金瓜石、九份、水湳洞的生活風景</p>
+            </div>
             <button>
                 <Link to="/articles">立即探索地方故事</Link>
             </button>
